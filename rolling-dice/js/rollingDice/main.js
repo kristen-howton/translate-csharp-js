7ec5f4ca143834a96c1eb59ca2400e03e94f1c84
Kristen Howton <kristen.howton@gmail.com>
2020-05-25 15:13:08 -0500

    got it to log, but mine has numbers instead of ?

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,6 +1,26 @@
-function main() {
+const main = () => {
   // Put your code here
+  console.log("Let's roll some dice, baby!");
+  console.log("---------------------------");
+
+  const Roll = () => {
+    dieValue = Math.floor(Math.random() * 6) + 1
+    return dieValue
+  }
+
+  for (let i = 0; i < 10; i++) {
+    let die1 = Roll();
+    let die2 = Roll();
+
+    let message = `${die1} + ${die2} === ${die1 + die2}`;
+    if (die1 === die2) {
+      message += " DOUBLES!";
+    }
+    console.log(message);
+  }
+
 
 }
+
 
 main();
